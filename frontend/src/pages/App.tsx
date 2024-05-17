c3a77dfe4add95167e17e990c66ee1d96488835a
Ernest Duckworth IV <31933290+ernman37@users.noreply.github.com>
2024-05-17 16:59:55 -0600

    Ernman organize (#1)
    * update index.html
    
    * update page location
    
    * update for jest tests

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import '../styles/App.css';
-import Header from './Header';
-import ExampleButton from './ExampleButton';
-import axios from 'axios';
+import Header from '../components/Header';
+import ExampleButton from '../components/ExampleButton';
 
 function App() {
   
